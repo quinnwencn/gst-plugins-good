e6260f29388f846232c4fb5bebc95bde77cd2610
Ming Qian <ming.qian@nxp.com>
2022-06-10 15:41:59 +0800

    MMFMWK-9186-7 v4l2: Add support for Y312
    This foramt is used by imx-jpeg

--- sys/v4l2/gstv4l2object.c
+++ sys/v4l2/gstv4l2object.c
@@ -154,6 +154,7 @@ static const GstV4L2FormatDesc gst_v4l2_formats[] = {
   {V4L2_PIX_FMT_HM12, TRUE, GST_V4L2_RAW},
   {V4L2_PIX_FMT_M420, TRUE, GST_V4L2_RAW},
   {V4L2_PIX_FMT_Y212, TRUE, GST_V4L2_RAW},
+  {V4L2_PIX_FMT_Y312, TRUE, GST_V4L2_RAW},
 
   /* two planes -- one Y, one Cr + Cb interleaved  */
   {V4L2_PIX_FMT_NV12, TRUE, GST_V4L2_RAW},
@@ -1100,6 +1101,7 @@ gst_v4l2_object_format_get_rank (const struct v4l2_fmtdesc *fmt)
     case V4L2_PIX_FMT_P012:
     case V4L2_PIX_FMT_P012M:
     case V4L2_PIX_FMT_Y212:
+    case V4L2_PIX_FMT_Y312:
       rank = YUV_ODD_BASE_RANK;
       break;
 
@@ -1449,6 +1451,9 @@ gst_v4l2_object_v4l2fourcc_to_video_format (guint32 fourcc)
     case V4L2_PIX_FMT_Y212:
       format = GST_VIDEO_FORMAT_Y212_LE;
       break;
+    case V4L2_PIX_FMT_Y312:
+      format = GST_VIDEO_FORMAT_Y312_LE;
+      break;
     default:
       format = GST_VIDEO_FORMAT_UNKNOWN;
       break;
@@ -1616,6 +1621,7 @@ gst_v4l2_object_v4l2fourcc_to_bare_struct (guint32 fourcc)
     case V4L2_PIX_FMT_P012:
     case V4L2_PIX_FMT_P012M:
     case V4L2_PIX_FMT_Y212:
+    case V4L2_PIX_FMT_Y312:
     case V4L2_PIX_FMT_YVU410:
     case V4L2_PIX_FMT_YUV410:
     case V4L2_PIX_FMT_YUV420:  /* I420/IYUV */
@@ -1932,6 +1938,9 @@ gst_v4l2_object_get_caps_info (GstV4l2Object * v4l2object, GstCaps * caps,
         break;
       case GST_VIDEO_FORMAT_Y212_LE:
         fourcc = V4L2_PIX_FMT_Y212;
+        break;
+      case GST_VIDEO_FORMAT_Y312_LE:
+        fourcc = V4L2_PIX_FMT_Y312;
         break;
       case GST_VIDEO_FORMAT_RGB15:
         fourcc = V4L2_PIX_FMT_RGB555;
